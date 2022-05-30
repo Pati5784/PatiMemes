54c9b3516f957c13f0c3caa8f0edd446b931a158
Pati5784 <pszczesnagawrylak@gmail.com>
2022-05-30 19:34:14 +0200

    prettier

--- src/patiMemes/Memes.js
+++ src/patiMemes/Memes.js
@@ -1,20 +1,20 @@
-import { Meme } from './Meme';
-import { Typography } from '@mui/material';
+import { Meme } from "./Meme";
+import { Typography } from "@mui/material";
 
 export function Memes(props) {
-	return (
-		<div>
-			<Typography variant='h4' gutterBottom component='div'>
-				{props.title}
-			</Typography>
-			{props.memes.map((item) => (
-				<Meme
-					{...item}
-					upvote={props.upvote}
-					downvote={props.downvote}
-					key={item.title}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom component="div">
+        {props.title}
+      </Typography>
+      {props.memes.map((item) => (
+        <Meme
+          {...item}
+          upvote={props.upvote}
+          downvote={props.downvote}
+          key={item.title}
+        />
+      ))}
+    </div>
+  );
 }
